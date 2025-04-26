828a9f4d0d04c8a400e730de1f6d6d1603ceb415
getmahbub <64847914+getmahbub@users.noreply.github.com>
2025-04-26 15:55:58 +1000

    CF update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from '@astrojs/cloudflare';
+
 // Import the Vercel adapter
 // import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://maintenance.gogeeks.au/",
+
+  //  output: 'server',
+  //  adapter: vercel(),
   integrations: [tailwind()],
-//  output: 'server',
-//  adapter: vercel(),
+
+  adapter: cloudflare(),
 });
